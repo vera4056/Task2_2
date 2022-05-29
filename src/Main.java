ad80048813119ebad334c23bb5caf93f1766e701
vera4056 <103599166+vera4056@users.noreply.github.com>
2022-05-29 19:46:45 +0300

    modified totalBalance

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 400;
         int deposit = 1300;
         int currentBalance = initialBalance + deposit;
         int bonus = currentBalance / 100; // 100 %
-        int y = bonus;
-        if (currentBalance > 1001) y = bonus;
-        if (currentBalance < 1001) {
-            y = 0;
+        if (currentBalance > 1001) {
+            bonus = currentBalance / 100;
         }
-        System.out.println("Итоговый баланс =" + currentBalance);
-        System.out.println("Бонус =" + y);
+        if (currentBalance < 1001) {
+            bonus = 0;
+        }
+        int totalBalance = currentBalance + bonus;
+        for (String s : Arrays.asList("Итоговый баланс =" + totalBalance, "Бонус =" + bonus)) {
+            System.out.println(s);
+        }
 
 
     }
